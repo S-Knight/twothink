f97b247bd585dafa101c9b78c4790fc9371065c0
S-Knight <360776816@qq.com>
2016-08-04 20:42:56 +0800

    基本完成了twothink的安装模块

--- code/application/install/controller/Index.php
+++ code/application/install/controller/Index.php
@@ -3,11 +3,27 @@ namespace app\install\controller;
 
 use think\Controller;
 
-class Index extends Controller
-{
-    public function index()
-    {
+class Index extends Controller{
+    public function index() {
         return $this->fetch('index');
     }
 
+    //安装完成
+    public function complete() {
+        $step = session('step');
+
+        if (!$step) {
+            $this->redirect('index');
+        } elseif ($step != 3) {
+            $this->redirect("Install/step{$step}");
+        }
+
+        // 写入安装锁定文件
+        file_put_contents(DATA_PATH . 'install.lock', date('Y-m-d H:i:s'));
+
+        session('step', NULL);
+        session('error', NULL);
+        session('update', NULL);
+        return $this->fetch();
+    }
 }
