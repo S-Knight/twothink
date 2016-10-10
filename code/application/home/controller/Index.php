98e7562e1b0d013f64fa37b1c2e2661a442968c5
S-Knight <360776816@qq.com>
2016-10-10 11:42:28 +0800

    完善了细节

--- code/application/home/controller/Index.php
+++ code/application/home/controller/Index.php
@@ -2,17 +2,15 @@
 namespace app\home\controller;
 use think\Controller;
 use think\Db;
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2016/8/11
- * Time: 15:27
- */
-class Index extends Controller
-{
-		public function index(){
 
-		    	$view = new \think\View();
-		    	return $view->fetch();
-		}
+class Index extends Controller{
+    public function index() {
+        if (!is_file(DATA_PATH . 'install.lock')) {
+            $this->redirect('install/Index/index');
+        }
+
+        $view = new \think\View();
+
+        return $view->fetch();
+    }
 }
