782db768cf7b35a0a0d90bfeb3f8aa2d613b9841
陈凯 <380883069@qq.com>
2017-02-04 18:11:30 +0800

    环节搭建htaccess/后台图片路径/

--- code/public/admin/template1/pages/scripts/login-4.js
+++ code/public/admin/template1/pages/scripts/login-4.js
@@ -255,10 +255,10 @@ var Login = function () {
 
             // init background slide images
 		    $.backstretch([
-		        "../assets/pages/media/bg/1.jpg",
-		        "../assets/pages/media/bg/2.jpg",
-		        "../assets/pages/media/bg/3.jpg",
-		        "../assets/pages/media/bg/4.jpg"
+		        "/admin/template1/pages/media/bg/1.jpg",
+		        "/admin/template1/pages/media/bg/2.jpg",
+		        "/admin/template1/pages/media/bg/3.jpg",
+		        "/admin/template1/pages/media/bg/4.jpg"
 		        ], {
 		          fade: 1000,
 		          duration: 8000
