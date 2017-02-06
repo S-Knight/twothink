8bfb2339099ee190ee29d4bfa29d655f17fe2fb4
S-Knight <360776816@qq.com>
2017-02-06 15:07:59 +0800

    优化完善了代码

--- code/application/admin/controller/Admin.php
+++ code/application/admin/controller/Admin.php
@@ -2,83 +2,41 @@
 namespace app\admin\controller;
 
 use app\admin\model\MenuModel;
-use app\admin\model\UcenterAdminModel;
 use think\Controller;
-use think\Session;
-use think\db;
 
-class Admin extends Controller{
-    protected $view = NULL;
+abstract class Admin extends Controller
+{
+    protected $view = null;
 
-    public function _initialize() {
-        $this->isLogin();
+    protected function _initialize()
+    {
+        if (!$this->isLogin()) {
+            $this->redirect('Account/login');
+        }
 
-        $firstMenus = MenuModel::all([
-            'pid'=>0,
+        $topMenus = MenuModel::all([
+            'pid' => 0,
             'hide' => 0
         ]);
-        foreach ($firstMenus as $firstMenu){
-            $firstMenu['child'] = MenuModel::all([
-                'pid'=>$firstMenu['id'],
+        foreach ($topMenus as $topMenu) {
+            $topMenu['child'] = MenuModel::all([
+                'pid' => $topMenu['id'],
                 'hide' => 0
             ]);
         }
-        //基本信息
-        $row['copyright'] = Db::table('geek_config')
-            ->where('name', 'Copyright_information')
-            ->value('value');
-        $row['alias'] = Db::table('geek_config')
-            ->where('name', 'alias')
-            ->value('value');
-        $this->assign("row", $row);
+
         $this->assign("admin", session('admin'));
-        $this->assign("firstMenus", $firstMenus);
-        $this->assign("username", session("user"));
+        $this->assign("topMenus", $topMenus);
     }
 
     /**
-     *判断是否登录
+     * 判断当前用户是否登录
+     * @return bool
      */
-    public function isLogin() {
-        $user = Session::get('admin');
-        if (!$user) {
-            $this->redirect('Account/login');
-        }
-    }
+    private function isLogin()
+    {
+        $user = session('admin');
 
-    public function menuDisplay() {
-        $menuModel = new Menu();
-
-        $firstMenus = $menuModel->where(['pid' => 0, 'hid' => 0])
-            ->order('sort')
-            ->select();
-
-        foreach ($firstMenus as &$firstMenu) {
-            $secondMenus = $menuModel->where([
-                'pid' => $firstMenu['id'],
-                'hid' => 0
-            ])->order('sort')->select();
-
-            foreach ($secondMenus as &$secondMenu) {
-                $thirdMenus = $menuModel->where([
-                    'pid' => $secondMenu['id'],
-                    'hid' => 0
-                ])->order('sort')->select();
-                $secondMenu['childmenu'] = $thirdMenus;
-            }
-            $firstMenu['childmenu'] = $secondMenus;
-        }
-    }
-
-    public function file($filename) {
-        $file = request()->file($filename);
-        if (!empty($file)) {
-            $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
-            if ($info) {
-                return $row = '/uploads/' . ($info->getSaveName());
-            } else {
-                $this->error($file->getError());
-            }
-        }
+        return $user ? true : false;
     }
 }
