c2b07f89353e3ba923cd454b9e68836293acb8eb
严超 <786040744@qq.com>
2017-03-25 20:25:06 +0800

    富文本插件

--- application/admin/common.php
+++ application/admin/common.php
@@ -89,103 +89,31 @@ function checkRolePerm($perm,$id)
 	return \app\admin\logic\PrivilegeLogic::checkRolePerm($perm, $id);
 }
 
-function newRichText($name){
+function newRichText($name,$value=''){
 	$a = <<<TexT
-<script src="/admin/template1/global/plugins/jquery.min.js" type="text/javascript"></script>
-<script type="text/javascript" src="/static/ueditor/ueditor.config.js"></script>
-<script type="text/javascript" src="/static/ueditor/ueditor.all.min.js"></script>
-<script type="text/javascript" src="/static/ueditor/lang/zh-cn/zh-cn.js"></script>
+<script id="{$name}" name='{$name}' type="text/plain" style='width:700px;'></script>
 <script type="text/javascript">
-    var ueditoroption = {
-        'autoClearinitialContent': false,
-        'toolbars': [['fullscreen', 'source', 'preview', '|', 'bold', 'italic', 'underline', 'strikethrough', 'forecolor', 'backcolor', '|',
-            'justifyleft', 'justifycenter', 'justifyright', '|', 'insertorderedlist', 'insertunorderedlist', 'blockquote', 'emotion', 'insertvideo',
-            'link', 'removeformat', '|', 'rowspacingtop', 'rowspacingbottom', 'lineheight', 'indent', 'paragraph', 'fontsize', '|',
-            'inserttable', 'deletetable', 'insertparagraphbeforetable', 'insertrow', 'deleterow', 'insertcol', 'deletecol',
-            'mergecells', 'mergeright', 'mergedown', 'splittocells', 'splittorows', 'splittocols', '|', 'anchor', 'map', 'print', 'drafts']],
-        'elementPathEnabled': false,
-        'initialFrameHeight': 200,
-        'initialFrameWidth': 600,
-        'focus': false,
-        'maximumWords': 9999999999999
-    };
-    var opts = {
-        type: 'image',
-        direct: false,
-        multi: true,
-        tabs: {
-            'upload': 'active',
-            'browser': '',
-            'crawler': ''
-        },
-        path: '',
-        dest_dir: '',
-        global: false,
-        thumb: false,
-        width: 0
-    };
-    UE.registerUI('myinsertimage', function (editor, uiName) {
-        editor.registerCommand(uiName, {
-            execCommand: function () {
-                require(['fileUploader'], function (uploader) {
-                    uploader.show(function (imgs) {
-                        if (imgs.length == 0) {
-                            return;
-                        } else if (imgs.length == 1) {
-                            editor.execCommand('insertimage', {
-                                'src': imgs[0]['url'],
-                                '_src': imgs[0]['attachment'],
-                                'width': '100%',
-                                'alt': imgs[0].filename
-                            });
-                        } else {
-                            var imglist = [];
-                            for (i in imgs) {
-                                imglist.push({
-                                    'src': imgs[i]['url'],
-                                    '_src': imgs[i]['attachment'],
-                                    'width': '100%',
-                                    'alt': imgs[i].filename
-                                });
-                            }
-                            editor.execCommand('insertimage', imglist);
-                        }
-                    }, opts);
-                });
-            }
-        });
-        var btn = new UE.ui.Button({
-            name: '插入图片',
-            title: '插入图片',
-            cssRules: 'background-position: -726px -77px',
-            onclick: function () {
-                editor.execCommand(uiName);
-            }
-        });
-        editor.addListener('selectionchange', function () {
-            var state = editor.queryCommandState(uiName);
-            if (state == -1) {
-                btn.setDisabled(true);
-                btn.setChecked(false);
-            } else {
-                btn.setDisabled(false);
-                btn.setChecked(state);
-            }
-        });
-        return btn;
-    }, 19);
+    var ue{$name} = UE.getEditor('{$name}');
+    ue{$name}.ready(function() {
+    
+    	ue{$name}.setContent('{$value}');
 
-    $(function () {
-        var ue = UE.getEditor("$name", ueditoroption);
-        $("#$name").data('editor', ue);
-        $("#$name").parents('form').submit(function () {
-            if (ue.queryCommandState('source')) {
-                ue.execCommand('source');
-            }
-        });
-    });
+	});
+    
+    function createEditor() {
+        enableBtn();
+        UE.getEditor('{$name}');
+    }
+    function enableBtn() {
+        var div = document.getElementById('btns');
+        var btns = UE.dom.domUtils.getElementsByTagName(div, "button");
+        for (var i = 0, btn; btn = btns[i++];) {
+            UE.dom.domUtils.removeAttributes(btn, ["disabled"]);
+        }
+    }
+    
+    createEditor()
 </script>
-        
 TexT;
 	return $a;
 }
