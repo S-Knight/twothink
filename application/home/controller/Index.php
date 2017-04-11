a66eb5e3721fc61c3c404f805d716d7835bf311c
严超 <786040744@qq.com>
2017-04-11 14:21:20 +0800

    as

--- application/home/controller/Index.php
+++ application/home/controller/Index.php
@@ -3,6 +3,9 @@ namespace app\home\controller;
 
 class Index extends Home{
     public function index() {
+        if (!is_file(DATA_PATH . 'install.lock')) {
+            $this->redirect('install/Index/index');
+        }
         $view = new \think\View();
         return $view->fetch('Index/index',[],['__HOME__'=>'/home/']);
     }
