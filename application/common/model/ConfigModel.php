c31ea91774ab82167b534dd65a7e776844a527a2
knight <360776816@qq.com>
2017-10-10 15:15:35 +0800

    Model使用时间戳功能

--- application/common/model/ConfigModel.php
+++ application/common/model/ConfigModel.php
@@ -6,17 +6,8 @@ use think\Model;
 
 class ConfigModel extends Model
 {
-    protected $insert = ['created_at'];
-    protected $update = ['updated_at'];
+    protected $createTime = 'created_at';
+    protected $updateTime = 'updated_at';
+    protected $autoWriteTimestamp = 'datetime';
     protected $table = 'geek_config';
-
-    protected function setCreatedAtAttr()
-    {
-        return date('Y-m-d H:i:s');
-    }
-
-    protected function setUpdatedAtAttr()
-    {
-        return date('Y-m-d H:i:s');
-    }
 }
