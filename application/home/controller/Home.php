cb8cec022ebc34cc5c7e830383bef14be3d4333b
knight <360776816@qq.com>
2017-10-17 16:03:16 +0800

    精简代码

--- application/home/controller/Home.php
+++ application/home/controller/Home.php
@@ -1,20 +1,18 @@
 <?php
+
 namespace app\home\controller;
+
 use think\Controller;
 
-class Home extends Controller{
-    protected $beforeActionList = ['checkDb','checkSite'];
-
-    protected function checkDb ()
+class Home extends Controller
+{
+    protected function _initialize()
     {
         if (!is_file(DATA_PATH . 'install.lock')) {
             $this->redirect('install/Index/index');
         }
-    }
 
-    protected function checkSite ()
-    {
-        if(getConfig('GUANBIZHANDI')){
+        if (getConfig('GUANBIZHANDI')) {
             $this->redirect('home/Error/index');
         }
     }
--- application/home/controller/Index.php
+++ application/home/controller/Index.php
@@ -1,12 +1,11 @@
 <?php
+
 namespace app\home\controller;
 
-class Index extends Home{
-    public function index() {
-        if (!is_file(DATA_PATH . 'install.lock')) {
-            $this->redirect('install/Index/index');
-        }
-        $view = new \think\View();
-        return $view->fetch('Index/index',[],['__HOME__'=>'/home/']);
+class Index extends Home
+{
+    public function index()
+    {
+        return view('Index/index');
     }
 }
