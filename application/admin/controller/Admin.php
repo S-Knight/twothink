4097bf831516466ce27d695890afe3f193bd17d4
knight <360776816@qq.com>
2017-12-08 16:28:14 +0800

    完善了菜单的权限管控

--- application/admin/controller/Admin.php
+++ application/admin/controller/Admin.php
@@ -17,7 +17,6 @@ abstract class Admin extends Controller
         $module = $this->request->module();
         $controller = $this->request->controller();
         $action = $this->request->action();
-
         $code = $module . "." . $controller . "." . $action;
 
         $admin = session('admin');
@@ -39,19 +38,36 @@ abstract class Admin extends Controller
         if (!$this->isLogin()) {
             $this->redirect('Account/login');
         }
-
         $menuModel = new MenuModel();
         $topMenus = $menuModel->where([
             'pid' => 0,
             'hide' => 0
         ])->order('sort')->select();
-        foreach ($topMenus as $topMenu) {
-            $topMenu['child'] = $menuModel->where([
+
+        foreach ($topMenus as $key => $topMenu) {
+            $topMenus[$key]['child'] = $menuModel->where([
                 'pid' => $topMenu['id'],
                 'hide' => 0
             ])->order('sort')->select();
+            /*
+             * 遍历一下当前菜单的二级菜单，判断当前登录用户是否有二级菜单的相关权限
+             * 如果没有任何二级菜单的权限，则过滤掉该一级菜单
+             */
+            $show = false;
+            foreach ($topMenus[$key]['child'] as $ckey => $child) {
+                if (checkRolePerm($child['privilege_code'],
+                    session('admin')['role_id'])) {
+                    //结果为真显示列表集
+                    $show = true;
+                } else {
+                    //结果为假把对象值设置为空对象,就不会显示列表集
+                    $child[$ckey] = null;
+                }
+            }
+            if (!$show) {
+                unset($topMenus[$key]);
+            }
         }
-
         $this->assign("admin", session('admin'));
         $this->assign("title", '后台管理系统');
         $this->assign("topMenus", $topMenus);
@@ -61,10 +77,23 @@ abstract class Admin extends Controller
      * 判断当前用户是否登录
      * @return bool
      */
-    private function isLogin()
+    protected function isLogin()
     {
         $user = session('admin');
 
         return $user ? true : false;
+    }
+
+    /**
+     * 获取当前登录用户id
+     * @return bool
+     */
+    protected function getLoginUid()
+    {
+        if (!$this->isLogin()) {
+            return false;
+        }
+
+        return session('admin')['id'];
     }
 }
